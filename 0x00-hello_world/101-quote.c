9b2b5451744c44c2295e9df4c17af3d86e2006a9
iginla adeyinka <iginlaadeyinka@gmail.com>
2023-09-11 16:22:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - this prints exactly the input on it
-* Return: always 1.
-*/
+ * main - Write
+ * a new line to the standard error
+ * Return: 0 (sucess)
+ */
 int main(void)
-{		
+{
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
