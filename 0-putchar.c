d0a81b1f81bb354282838d86d987baffbac32413
iginla adeyinka <iginlaadeyinka@gmail.com>
2023-09-14 13:56:29 -0700

    more functions

---
+++ 0-putchar.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ * main - print _putchar
+ * writtrn by me
+ * Return )
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
