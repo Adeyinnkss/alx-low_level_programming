4d7aa6aeb2ebbc622238daf8bbf22fd5aff4a6f6
iginla adeyinka <iginlaadeyinka@gmail.com>
2023-09-14 17:58:37 -0700

    main.c

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio>
 /**
 * main - Test function for positive or negative
 * Return: 0
@@ -6,8 +7,7 @@
 int main(void)
 {
 int i;
-i = 0;
+i = 0
 positive_or_negative(i);
 return (0);
 }
-
